46d343edadad4d353011e20e7971944f214f63e6
gen <kyliki@yandex.ru>
2023-07-08 01:34:16 +0300

    component search render

--- src/components/rolodex/Rolodex.tsx
+++ src/components/rolodex/Rolodex.tsx
@@ -1,9 +1,11 @@
 import TitlePage from '../TitlePage';
+import RolodexSearch from './RolodexSearch';
 
 export default function Rolodex() {
   return (
     <div className="content">
       <TitlePage />
+      <RolodexSearch />
     </div>
   );
 }
---
+++ src/components/rolodex/RolodexSearch.tsx
@@ -0,0 +1,59 @@
+import {
+  Button,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+} from 'antd';
+import { useState } from 'react';
+
+export default function RolodexSearch() {
+  const [datePickerFocused, setDatePickerFocused] = useState(false);
+
+  function handelFocusDatePicker(isFocused: boolean):void {
+    setDatePickerFocused(isFocused);
+    // TODO сделать проверку на наличие value
+  }
+
+  return (
+    <div className="wrap-form">
+      <Form layout="inline" colon={false} className="ant-form--rolodex">
+
+        <Form.Item label="Пациент" className="ant-form-item--patient">
+          <Input />
+        </Form.Item>
+
+        <Form.Item label="№ Амб. карты" className="ant-form-item--card-number">
+          <InputNumber />
+        </Form.Item>
+
+        <Form.Item>
+          <DatePicker
+            placeholder=""
+            onFocus={() => handelFocusDatePicker(true)}
+            onBlur={() => handelFocusDatePicker(false)}
+          />
+          <span
+            className={
+              datePickerFocused
+                ? 'ant-form__label ant-form__label--active'
+                : 'ant-form__label'
+            }
+          >
+            Дата рождения
+          </span>
+        </Form.Item>
+
+        <div className="ant-form__group">
+          <Form.Item>
+            <Button type="primary">Найти</Button>
+          </Form.Item>
+
+          <Form.Item>
+            <Button className="ant-btn-secondary">Новый пациент</Button>
+          </Form.Item>
+        </div>
+      </Form>
+    </div>
+  );
+}
