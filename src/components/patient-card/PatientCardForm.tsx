590aaddaee893a09bcb2ddde570aacd5b53d6050
gen <kyliki@yandex.ru>
2023-07-09 13:09:35 +0300

    fix radio-button, button-natural

--- src/components/patient-card/PatientCardForm.tsx
+++ src/components/patient-card/PatientCardForm.tsx
@@ -1,5 +1,5 @@
 import {
-  // Button,
+  Button,
   DatePicker,
   Form,
   Input,
@@ -8,6 +8,7 @@ import {
   Select,
   Col,
   Row,
+  Divider,
 } from 'antd';
 import type { SelectProps, RadioChangeEvent } from 'antd';
 import { useState } from 'react';
@@ -33,6 +34,7 @@ const optionsBloodType:SelectProps['options'] = [
 
 export default function PatientCardForm() {
   const [patientSex, setPatientSex] = useState(1);
+  const [isPickerFocused, setIsPickerFocused] = useState(false);
 
   const onChangePatientSex = ({ target: { value } }: RadioChangeEvent) => {
     setPatientSex(value);
@@ -46,10 +48,17 @@ export default function PatientCardForm() {
     console.log(value);
   };
 
+  function handelFocusDatePicker(isFocused: boolean):void {
+    setIsPickerFocused(isFocused);
+    console.log(isPickerFocused);
+
+    // TODO сделать проверку на наличие value
+  }
+
   return (
     <div className="wrap-form wrap-form--patient-card">
-      <Form layout="horizontal" colon={false}>
-        <Row>
+      <Form layout="horizontal" colon={false} className="ant-form--custom">
+        <Row gutter={[31, 35]}>
           <Col span={5}>
             <Form.Item>
               <Input placeholder="Фамилия" />
@@ -68,18 +77,36 @@ export default function PatientCardForm() {
             </Form.Item>
           </Col>
 
-          <Col span={5}>
-            <Form.Item>
-              <DatePicker />
+          <Col span={6}>
+            <Form.Item className={
+              isPickerFocused
+                ? 'ant-picker--active ant-picker--grand'
+                : 'ant-picker--grand'
+            }
+            >
+              <DatePicker
+                placeholder="Дата рождения"
+                onFocus={() => handelFocusDatePicker(true)}
+                onBlur={() => handelFocusDatePicker(false)}
+              />
+              <span
+                className={
+                  isPickerFocused
+                    ? 'ant-form__label ant-form__label--active'
+                    : 'ant-form__label'
+                }
+              >
+                Дата рождения
+              </span>
             </Form.Item>
           </Col>
 
-          <Col span={4}>
+          <Col span={3}>
             <div className="client-age">we</div>
           </Col>
         </Row>
 
-        <Row>
+        <Row gutter={[31, 35]} className="ant-row--vertical-centred">
           <Col span={5}>
             <Form.Item label="Пол*" className="form-item-custom">
               <Radio.Group
@@ -88,6 +115,7 @@ export default function PatientCardForm() {
                 value={patientSex}
                 optionType="button"
                 buttonStyle="solid"
+
               />
             </Form.Item>
           </Col>
@@ -107,12 +135,13 @@ export default function PatientCardForm() {
                 placeholder="Группа крови"
                 onChange={handleChangeBloodType}
                 options={optionsBloodType}
+
               />
             </Form.Item>
           </Col>
         </Row>
 
-        <Row>
+        <Row gutter={[31, 35]} className="ant-row--vertical-centred">
           <Col span={5}>
             <Form.Item>
               <Input placeholder="Рост" />
@@ -129,9 +158,85 @@ export default function PatientCardForm() {
                 placeholder="Цвет глаз"
                 onChange={handleChangeEyesColor}
                 options={optionsEyesColor}
+
               />
             </Form.Item>
           </Col>
+        </Row>
+
+        <Divider orientation="left">Адрес регистрации</Divider>
+
+        <Row gutter={[31, 35]} className="ant-row--vertical-centred">
+          <Col span={5}>
+            <Form.Item>
+              <Input placeholder="Полный адрес" />
+            </Form.Item>
+          </Col>
+
+          <Col span={5}>
+            <Form.Item>
+              <Input placeholder="Город" />
+            </Form.Item>
+          </Col>
+
+          <Col span={5}>
+            <Form.Item>
+              <Input placeholder="Почтовый индекс" />
+            </Form.Item>
+          </Col>
+
+        </Row>
+
+        <Divider orientation="left">Место работы</Divider>
+
+        <Row gutter={[31, 35]} className="ant-row--vertical-centred">
+          <Col span={5}>
+            <Form.Item>
+              <Input placeholder="Полный адрес" />
+            </Form.Item>
+          </Col>
+
+          <Col span={5}>
+            <Form.Item>
+              <Input placeholder="Город" />
+            </Form.Item>
+          </Col>
+
+          <Col span={5}>
+            <Form.Item>
+              <Input placeholder="Почтовый индекс" />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={[31, 35]} className="ant-row--vertical-centred">
+          <Col span={5}>
+            <Form.Item>
+              <Input placeholder="Компания" />
+            </Form.Item>
+          </Col>
+
+          <Col span={5}>
+            <Form.Item>
+              <Input placeholder="Отдел" />
+            </Form.Item>
+          </Col>
+
+          <Col span={5}>
+            <Form.Item>
+              <Input placeholder="Должность" />
+            </Form.Item>
+          </Col>
+
+          <Col span={4} offset={5}>
+            <div className="button-wrap">
+              <Button className="ant-btn-natural">Назад</Button>
+              <Button type="primary" danger>Удалить</Button>
+              <Button className="ant-btn-secondary">Сохранить</Button>
+            </div>
+
+          </Col>
+
         </Row>
       </Form>
     </div>
